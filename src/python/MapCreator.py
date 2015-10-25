e6e48fb343466fb8357df830de9c4a27786aa461
Meinolf Vogt <mevo1961@gmail.com>
2015-10-25 19:06:55 +0100

    added some code and unittest

--- src/python/MapCreator.py
+++ src/python/MapCreator.py
@@ -5,6 +5,8 @@ Created on 25.10.2015
 '''
 
 from argparse import ArgumentParser
+import sys
+import shlex
 
 class MapCreator(object):
     '''
@@ -12,35 +14,47 @@ class MapCreator(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self, options):
         '''
         Constructor
         '''
-        self.__cmdArgs = self.__parseCmd()
+        # self.__cmdArgs = self.parseCmd(sys.argv[1:])
+        self.__cmdArgs = self.parseCmd(options)
     
-    def __parseCmd(self):
-        parser = ArgumentParser(description='Create a Garmin map from OpenStreetMap data')
+    def parseCmd(self, options):
+        # parser = ArgumentParser(description='Create a Garmin map from OpenStreetMap data')
+        parser = ArgumentParser()
 
         mandatory = parser.add_argument_group('mandatory arguments')
 
         helpMsg = 'bottom coordinate of the area to cut from input data'
-        parser.add_argument('-b', '--bottom', action='store', type=str, dest='bottom',
+        parser.add_argument('-b', '--bottom', action='store', type=float, dest='bottom',
                             required=False, help=helpMsg)
         
         helpMsg = 'top coordinate of the area to cut from input data'
-        parser.add_argument('-t', '--top', action='store', type=str, dest='top',
+        parser.add_argument('-t', '--top', action='store', type=float, dest='top',
                             required=False, help=helpMsg)
         
         helpMsg = 'bottom coordinate of the area to cut from input data'
-        parser.add_argument('-l', '--left', action='store', type=str, dest='left',
+        parser.add_argument('-l', '--left', action='store', type=float, dest='left',
                             required=False, help=helpMsg)
         
         helpMsg = 'bottom coordinate of the area to cut from input data'
-        parser.add_argument('-r', '--right', action='store', type=str, dest='right',
+        parser.add_argument('-r', '--right', action='store', type=float, dest='right',
                             required=False, help=helpMsg)
 
 
 
         helpMsg = 'input file containing OpenStreetMap data'
-        mandatory.add_argument('-i', '--input', action='store', type=str, dest='inputdir',
-                               metavar='INPUT', required=True, help=helpMsg) 
+        mandatory.add_argument('-i', '--input', action='store', type=str, dest='inputfile',
+                               required=True, help=helpMsg)
+        
+        args = parser.parse_args(options)
+        return args
+    
+    def getArgs(self):
+        return self.__cmdArgs
+    
+if __name__ == "__main__":
+    args = MapCreator(sys.argv[1:]).getArgs()
+    print ("inputfile = %s") % (args.inputfile)
--- test/python/MapCreator_test.py
+++ test/python/MapCreator_test.py
@@ -4,15 +4,16 @@ Created on 25.10.2015
 @author: mevo
 '''
 import unittest
-import MapCreator
+import shlex
+from MapCreator import MapCreator
 
 
 class Test(unittest.TestCase):
 
 
     def setUp(self):
-        self.creator=MapCreator()
-        pass
+        argString = '-b 10 -t 11 -l 12 -r 13 -i input.txt'
+        self.creator=MapCreator(shlex.split(argString))
 
 
     def tearDown(self):
@@ -20,7 +21,9 @@ class Test(unittest.TestCase):
 
 
     def testParseCmd(self):
-        pass
+        argString = '-b 10 -t 11 -l 12 -r 13 -i input.txt'
+        cmdArgs = self.creator.getArgs()
+        self.assertEqual(10, cmdArgs.bottom, 'bottom was not parsed properly')
 
 
 if __name__ == "__main__":
