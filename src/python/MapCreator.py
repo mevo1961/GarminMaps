7f25882f06b792e51f91fae9fd2981ad5139e0a0
Meinolf Vogt <meinolf.vogt@gmx.de>
2015-10-28 19:31:04 +0100

    added remaining options

--- src/python/MapCreator.py
+++ src/python/MapCreator.py
@@ -20,27 +20,69 @@ class MapCreator(object):
         self.__cmdArgs = self.parseCmd(options)
     
     def parseCmd(self, options):
-        # parser = ArgumentParser(description='Create a Garmin map from OpenStreetMap data')
-        parser = ArgumentParser()
+        parser = ArgumentParser(description='Create a Garmin map from OpenStreetMap data')
+        # parser = ArgumentParser()
 
         mandatory = parser.add_argument_group('mandatory arguments')
 
+        # coordinate options
         helpMsg = 'bottom coordinate of the area to cut from input data'
         parser.add_argument('-b', '--bottom', action='store', type=float, dest='bottom',
-                            required=False, help=helpMsg)
+                            required=False, default=47.2, help=helpMsg)
         
         helpMsg = 'top coordinate of the area to cut from input data'
         parser.add_argument('-t', '--top', action='store', type=float, dest='top',
-                            required=False, help=helpMsg)
+                            required=False, default=55.2, help=helpMsg)
         
-        helpMsg = 'bottom coordinate of the area to cut from input data'
+        helpMsg = 'western coordinate of the area to cut from input data'
         parser.add_argument('-l', '--left', action='store', type=float, dest='left',
-                            required=False, help=helpMsg)
+                            required=False, default=5.7, help=helpMsg)
         
-        helpMsg = 'bottom coordinate of the area to cut from input data'
+        helpMsg = 'eastern coordinate of the area to cut from input data'
         parser.add_argument('-r', '--right', action='store', type=float, dest='right',
-                            required=False, help=helpMsg)
-
+                            required=False, default=15.1, help=helpMsg)
+        
+        # contourline options
+        helpMsg = 'distance between minor contour lines'
+        parser.add_argument('--ci', '--contour-step-min', action='store', type=int, dest='cstepmin',
+                            required=False, default=10, help=helpMsg)
+        
+        helpMsg = 'distance between medium contourlines'
+        parser.add_argument('--ce', '--contour-step-med', action='store', type=int, dest='cstepmed',
+                            required=False, default=50, help=helpMsg)
+        
+        helpMsg = 'distance between major contourlines'
+        parser.add_argument('--ca', '--contour-step-max', action='store', type=int, dest='cstepmax',
+                            required=False, default=100, help=helpMsg)
+        
+        # mapid and mapname options
+        helpMsg = 'map id (a unique 4 digit integer)'
+        parser.add_argument('--mi', '--map-id', action='store', type=int, dest='mapid',
+                            required=False, default=6400, help=helpMsg)
+        
+        helpMsg = 'mapname (a string giving a name to the map)'
+        parser.add_argument('--mn', '--map-name', action='store', type=str, dest='mapname',
+                            required=False, default='GER', help=helpMsg)
+        
+        # family ids for main map and contour maps
+        helpMsg = 'family id (a unique 4 digit integer)'
+        parser.add_argument('--fi', '--family-id', action='store', type=int, dest='famid',
+                            required=False, default=1441, help=helpMsg)
+        
+        helpMsg = 'minor contourmap family id (a unique 4 digit integer)'
+        parser.add_argument('--cid', '--contour-min-family-id', action='store', type=int, dest='cminid',
+                            required=False, default=2441, help=helpMsg)
+        
+        helpMsg = 'major contourmap family id (a unique 4 digit integer)'
+        parser.add_argument('--cad', '--contour-max-family-id', action='store', type=int, dest='cmaxid',
+                            required=False, default=2443, help=helpMsg)
+        
+        # nocontours flag
+        helpMsg = 'scan directories recursive'
+        parser.add_argument('--nc', '--no-contours', action='store_true', dest='nocontours',
+                            required=False, default=False, help=helpMsg)
+        
+        # inputfile option
         helpMsg = 'input file containing OpenStreetMap data'
         mandatory.add_argument('-i', '--input', action='store', type=str, dest='inputfile',
                                required=True, help=helpMsg)
--- test/python/MapCreator_test.py
+++ test/python/MapCreator_test.py
@@ -19,15 +19,30 @@ class Test(unittest.TestCase):
         pass
 
 
-    def testParseCmd_OK(self):
-        argString = '-b 10 -t 11 -l 12 -r 13 -i input.txt'
+    def testParseCmd_All_OK(self):
+        argString = '-b 40 -t 60 -l 5 -r 15 --ci 10 --ce 50 --ca 100 --mi 6400 --mn GER --fi 1441 --nc --no-contours -i input.txt'
         self.creator=MapCreator(shlex.split(argString))
         cmdArgs = self.creator.getArgs()
-        self.assertEqual(10,          cmdArgs.bottom,    'bottom was not parsed properly')
-        self.assertEqual(11,          cmdArgs.top,       'top was not parsed properly')
-        self.assertEqual(12,          cmdArgs.left,      'left was not parsed properly')
-        self.assertEqual(13,          cmdArgs.right,     'right was not parsed properly')
+        self.assertEqual(40,          cmdArgs.bottom,    'bottom was not parsed properly')
+        self.assertEqual(60,          cmdArgs.top,       'top was not parsed properly')
+        self.assertEqual(5,           cmdArgs.left,      'left was not parsed properly')
+        self.assertEqual(15,          cmdArgs.right,     'right was not parsed properly')
+        
+        self.assertEqual(10,          cmdArgs.cstepmin,  'minimum contour step was not parsed properly')
+        self.assertEqual(50,          cmdArgs.cstepmed,  'medium contour step was not parsed properly')
+        self.assertEqual(100,         cmdArgs.cstepmax,  'maximum contour step was not parsed properly')
+        
+        self.assertEqual(6400,        cmdArgs.mapid,     'mapid was not parsed properly')
+        self.assertEqual("GER",       cmdArgs.mapname,   'mapname was not parsed properly')
+        
+        self.assertEqual(1441,        cmdArgs.famid,     'family id was not parsed properly')
+        self.assertEqual(2441,        cmdArgs.cminid,    'minor contour map family id was not parsed properly')
+        self.assertEqual(2443,        cmdArgs.cmaxid,    'major contour map family id was not parsed properly')
+        
+        self.assertEqual(True,        cmdArgs.nocontours, 'nocontous flag was not parsed properly')
+           
         self.assertEqual('input.txt', cmdArgs.inputfile, 'inputfile was not parsed properly')
+        
 
 
 if __name__ == "__main__":
