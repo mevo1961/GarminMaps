7e76b4dd90b6b84eaaf5f28a34a84f259a80bec7
Meinolf Vogt <mevo1961@gmail.com>
2015-10-30 17:39:43 +0100

    added xml-results

--- test/python/MapCreator_test.py
+++ test/python/MapCreator_test.py
@@ -77,4 +77,5 @@ class Test(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
