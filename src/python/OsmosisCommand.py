623a37eba7b78351534254da01d71f04caba3098
Meinolf Vogt <meinolf.vogt@gmx.de>
2016-02-10 19:50:36 +0100

    add splitting of temp.osm into tiles:
    continued implementation
    Task-Url: http://github.com/mevo1961/GarminMaps/issues/issue/1

--- src/python/OsmosisCommand.py
+++ src/python/OsmosisCommand.py
@@ -21,24 +21,24 @@ class OsmosisCommand(object):
         self.__osmosisTool = os.path.join(self.__toolsDir, "osmosis/bin/osmosis")
           
     def cutMapWithPolygon(self, infile="germany.osm", outfile="temp.osm", poly="germany.poly"):
-        cmd = self.__osmosisTool                             + \
-              ' --read-xml file="'         + infile    + '"' + \
-              ' --bounding-polygon file="' + poly      + '"' + \
-              ' completeWays=no'                            + \
-              ' --write-xml file="'        + outfile         + '"'
+        datatype = self._getDataType(infile)
+        cmd = self.__osmosisTool                                      + \
+              ' --read-' + datatype        + ' file="' + infile + '"' + \
+              ' --bounding-polygon file="' + poly      + '"'          + \
+              ' completeWays=no'                                      + \
+              ' --write-' + datatype       + ' file="'  + outfile     + '"'
               
         return cmd
     
     def cutMapWithBoundingBox(self, infile="germany.osm", outfile="temp.osm",
                               top=55.2, left=5.7, bottom=47.2, right=15.1):
         datatype = self._getDataType(infile)
-        
         cmd = self.__osmosisTool                                            + \
-              ' --read-' + datatype + ' file="'   + infile      + '"'                    + \
-              ' --bounding-box top=' + str(top)    + ' left=' + str(left)   + \
+              ' --read-' + datatype  + ' file="'   + infile    + '"'        + \
+              ' --bounding-box top=' + str(top)    + ' left='  + str(left)  + \
               ' bottom='             + str(bottom) + ' right=' + str(right) + \
-              ' completeWays=no'                                           + \
-              ' --write-' + datatype + ' file="'  + outfile     + '"'
+              ' completeWays=no'                                            + \
+              ' --write-' + datatype + ' file="'   + outfile   + '"'
         return cmd
     
     def _getDataType(self, infile):
--- test/python/OsmosisCommand_test.py
+++ test/python/OsmosisCommand_test.py
@@ -34,27 +34,40 @@ class Test_OsmosisCommand(unittest.TestCase):
         expectedStr = self.__osmosisTool + ' --read-xml file="germany.osm" --bounding-polygon file="germany.poly" completeWays=no --write-xml file="temp.osm"'
         logging.debug('cmdstr   = %s' % cmdstr)
         logging.debug('expected = %s' % expectedStr)
-        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was: %s' % (expectedStr, cmdstr))
+        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was:  %s' % (expectedStr, cmdstr))
     
     def testCutMapWithPolygon_Parameters(self):
         cmdstr = self.__osmosisCmd.cutMapWithPolygon(infile="input.osm", outfile="output.osm", poly="borders.poly")
         expectedStr = self.__osmosisTool + ' --read-xml file="input.osm" --bounding-polygon file="borders.poly" completeWays=no --write-xml file="output.osm"'
         logging.debug('cmdstr   = %s' % cmdstr)
         logging.debug('expected = %s' % expectedStr)
-        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was: %s' % (expectedStr, cmdstr))
+        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was:  %s' % (expectedStr, cmdstr))
+        
+    def testCutMapWithPolygon_Parameters_pbf(self):
+        cmdstr = self.__osmosisCmd.cutMapWithPolygon(infile="input.osm.pbf", outfile="output.osm.pbf", poly="borders.poly")
+        expectedStr = self.__osmosisTool + ' --read-pbf file="input.osm.pbf" --bounding-polygon file="borders.poly" completeWays=no --write-pbf file="output.osm.pbf"'
+        logging.debug('cmdstr   = %s' % cmdstr)
+        logging.debug('expected = %s' % expectedStr)
+        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was:  %s' % (expectedStr, cmdstr))
         
     def testCutMapWithBoundingBox_Default(self):
         cmdstr = self.__osmosisCmd.cutMapWithBoundingBox()
         expectedStr = self.__osmosisTool + ' --read-xml file="germany.osm" --bounding-box top=55.2 left=5.7 bottom=47.2 right=15.1 completeWays=no --write-xml file="temp.osm"'
-        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was: %s' % (expectedStr, cmdstr))
-        
+        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was:  %s' % (expectedStr, cmdstr))
         
     def testCutMapWithBoundingBox_Parameters(self):
         cmdstr = self.__osmosisCmd.cutMapWithBoundingBox(top=49.5138, left=10.9351, bottom=49.3866, right=11.201)
         expectedStr = self.__osmosisTool + ' --read-xml file="germany.osm" --bounding-box top=49.5138 left=10.9351 bottom=49.3866 right=11.201 completeWays=no --write-xml file="temp.osm"'
         logging.debug('cmdstr   = %s' % cmdstr)
         logging.debug('expected = %s' % expectedStr)
-        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was: %s' % (expectedStr, cmdstr))
+        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was:  %s' % (expectedStr, cmdstr))
+        
+    def testCutMapWithBoundingBox_Parameters_pbf(self):
+        cmdstr = self.__osmosisCmd.cutMapWithBoundingBox(infile="germany.osm.pbf", top=49.5138, left=10.9351, bottom=49.3866, right=11.201, outfile="temp.osm.pbf")
+        expectedStr = self.__osmosisTool + ' --read-pbf file="germany.osm.pbf" --bounding-box top=49.5138 left=10.9351 bottom=49.3866 right=11.201 completeWays=no --write-pbf file="temp.osm.pbf"'
+        logging.debug('cmdstr   = %s' % cmdstr)
+        logging.debug('expected = %s' % expectedStr)
+        self.assertEqual(expectedStr, cmdstr,  'unexpected cmdstr, \nexpected: %s,\nbut was:  %s' % (expectedStr, cmdstr))
 
 if __name__ == "__main__":
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output = 'test-reports', outsuffix = time.strftime("%Y%m%d%H%M%S")))
